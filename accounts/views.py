2ceca42bea480c7f6161f3d53c95749b7e5a52f0
Jared Tangir <jaredtangir@Jareds-MBP-2.fios-router.home>
2020-05-29 21:15:06 -0400

    redirect to dashboard , hide subnote

--- accounts/views.py
+++ accounts/views.py
@@ -12,8 +12,7 @@ def register(request):
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect("home")
+            return redirect("dashboard")
     else:
         form = RegisterForm()
     return render(request, "accounts/register.html", {"form": form})
-
