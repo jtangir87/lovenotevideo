0865cb05877776dd0e05153e955a7b0f384ca7c4
Jared Tangir <jaredtangir@Jareds-MBP-2.fios-router.home>
2020-06-03 14:32:54 -0400

    add django-cleanup

--- lovenotevideo/settings.py
+++ lovenotevideo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "bootstrap4",
     "bootstrap_datepicker_plus",
+    "django_cleanup.apps.CleanupConfig",
     "videokit",
     "django_simple_coupons",
     "accounts",
